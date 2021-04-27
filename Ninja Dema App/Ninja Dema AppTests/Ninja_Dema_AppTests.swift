4cf79b91357d3d199883d8498bd7f649c92fec6f
XIC303-GaneshPrasad <ganesh.prasad_xeb@idfcfirstbank.com>
2021-04-27 14:33:44 +0530

    Rearranging the files setup

---
+++ Ninja Dema App/Ninja Dema AppTests/Ninja_Dema_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ninja_Dema_AppTests.swift
+//  Ninja Dema AppTests
+//
+//  Created by Ramniwas Patidar(Xebia) on 27/04/21.
+//  Copyright © 2021 Ninja Dema App. All rights reserved.
+//
+
+import XCTest
+@testable import Ninja_Dema_App
+
+class Ninja_Dema_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
