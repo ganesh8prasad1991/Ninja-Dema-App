6cc012dd512a2abb3dfc9c979497c05610f608c2
XIC303-GaneshPrasad <ganesh.prasad_xeb@idfcfirstbank.com>
2021-04-27 18:58:27 +0530

    Encryption/Decryption Logic added

--- Ninja Dema App/Ninja Dema App/Encrption & Decryption/CryptoViewController.swift
+++ Ninja Dema App/Ninja Dema App/Encrption & Decryption/CryptoViewController.swift
@@ -24,11 +24,57 @@ class CryptoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print(String(bytes: xorEncryption("Hello", encryptedKey: "World"), encoding: .utf8))
+        print(xorDecryption(xorEncryption("Hello", encryptedKey: "World"), decodingKey: "World"))
     }
     
     
     private func setup() {
         showNavBar()
     }
+    
+    
+    private func convertEncryptedDatatoString(_ encrptedData: [UInt8]) -> String {
+        return String(bytes: encrptedData, encoding: .utf8) ?? ""
+    }
+    
+    private func xorEncryption(_ message: String, encryptedKey: String) -> [UInt8] {
+        
+        if message.count == 0 { return [UInt8]() }
+        
+        var encrypted = [UInt8]()
+        let text = [UInt8](message.utf8)
+        let key = [UInt8](encryptedKey.utf8)
+        let length = key.count
+        
+        
+        // encrypt bytes
+        for t in text.enumerated() {
+            encrypted.append(t.element ^ key[t.offset % length])
+        }
+        
+        return encrypted
+    }
+    
+    
+    private func xorDecryption(_ cyperText: [UInt8], decodingKey: String) -> String {
+        
+        if cyperText.count == 0 {return ""}
+        
+        var decrypted = [UInt8]()
+        let cyper = cyperText
+        let key = [UInt8](decodingKey.utf8)
+        let length = key.count
+        
+        // decrypt bytes
+        for t in cyper.enumerated() {
+            decrypted.append(t.element ^ key[t.offset % length])
+        }
+        
+        return String(bytes: decrypted, encoding: .utf8) ?? ""
+    }
+    
     
 }
